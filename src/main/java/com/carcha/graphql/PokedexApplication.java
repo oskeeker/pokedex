720ffee42e3eab10d6eac6d918054d77764e26e0
Oscar <oscar.sanchez@lyngual.com>
2020-04-17 09:12:02 +0200

    Javadoc

--- src/main/java/com/carcha/graphql/PokedexApplication.java
+++ src/main/java/com/carcha/graphql/PokedexApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Random;
-
 /**
  * Main app
  */
